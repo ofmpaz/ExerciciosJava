d726460c40fffaad7b8815e7cf5a79a5fb116f29
Maria Andrade <deise_andrade_paz@hotmail.com>
2024-10-23 11:28:17 -0300

    LaçoWhile
    Recebendo nome e idade

---
+++ src/main/java/IntroducaoJava.java
@@ -0,0 +1,23 @@
+public class IntroducaoJava {
+
+    public static void main(String[] args) {
+
+        int inteiro;
+        double decimal;
+        float decimalMarior;
+        byte pequeno;
+        char unico;
+
+        String nome;
+        Double numero;
+        Integer numero2;
+        Float numero1;
+
+        inteiro = 1;
+
+        numero2 = 2;
+
+        Integer soma = numero2 + inteiro;
+
+    }
+}
